14108ba60beb3aefd3d889671dd1361182716c2b
mariocstweb <mariocstmra@gmail.com>
2024-01-23 15:08:15 +0100

    added tasks in data

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ console.log("ok", Vue);
 
 const { createApp } = Vue;
 const app = createApp({
-  data: () => ({}),
+  data: () => ({
+    tasks: [
+      { id: 1, done: false, text: "Fare la spesa" },
+      { id: 2, done: true, text: "Lavare i capelli" },
+      { id: 3, done: false, text: "Comprare una marca da bollo" },
+      { id: 4, done: false, text: "Aggiornare il PC" },
+    ],
+  }),
 });
 
 app.mount("#root");
